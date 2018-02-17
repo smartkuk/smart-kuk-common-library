68375e0c85cce858546345b29866a89810fc69db
SmartKuk <pipe0502@Smarts-MacBook-Pro.local>
2018-02-17 10:58:45 +0900

    setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# smart-kuk-common-library
---
+++ smart-kuk-common-library/smart_kuk_common_library.h
@@ -0,0 +1,19 @@
+//
+//  smart_kuk_common_library.h
+//  smart-kuk-common-library
+//
+//  Created by SmartKuk on 2/17/18.
+//  Copyright © 2018 SmartCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for smart_kuk_common_library.
+FOUNDATION_EXPORT double smart_kuk_common_libraryVersionNumber;
+
+//! Project version string for smart_kuk_common_library.
+FOUNDATION_EXPORT const unsigned char smart_kuk_common_libraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <smart_kuk_common_library/PublicHeader.h>
+
+
